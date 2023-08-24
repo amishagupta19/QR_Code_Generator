46e4a8ea90a5eb088c49eee44b67e88817b2ddb9
amishagupta19 <106989098+amishagupta19@users.noreply.github.com>
2023-08-25 02:27:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const inputData = document.getElementById('inputData');
+//     const generateButton = document.getElementById('generateButton');
+//     const qrCodeDiv = document.getElementById('qrcode');
+  
+//     generateButton.addEventListener('click', function () {
+//       const dataToEncode = inputData.value;
+//       if (dataToEncode.trim() !== '') {
+//         const qrCode = new QRCode(qrCodeDiv, {
+//           text: dataToEncode,
+//           width: 128,
+//           height: 128,
+//         });
+//       }
+//     });
+// });
+  
